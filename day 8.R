88164c5743362af58bed18407fd28d2fe0d386ae
James McMahon <j.mcmahon1@nhs.net>
2019-12-09 16:40:10 +0000

    Tidy up part 1

--- day 8.R
+++ day 8.R
@@ -12,26 +12,16 @@ split_to_layers <- function(string, width, height) {
   size <- width * height
 
   layer_list <- str_extract_all(string, paste0(".{", size, "}"), simplify = T) %>%
-    as.list %>% map(~str_split(.x, "", simplify = T))
+    as.list %>% map(~str_split(.x, "", simplify = T)) %>% map(as.integer)
 
   return(layer_list)
 }
 
-split_to_vec <- function(integer) {
-  digits <- ceiling(log10(integer))
-  vec_1 <- 10 ^ (digits:1)
-  vec_2 <- vec_1 / 10
-  final_vec <- (integer %% vec_1) %/% vec_2
-
-  return(final_vec)
-}
-
 layers <- input %>% split_to_layers(width = 25, height = 6)
 
-zero_count <- layers %>% map_int(~sum(.x == "0"))
+zero_count <- layers %>% map_int(~sum(.x == 0))
 
 fewest_zero_layer <- layers[which(min(zero_count) == zero_count)] %>% flatten()
 
-sum(fewest_zero_layer == "1") * sum(fewest_zero_layer == "2")
-
+sum(fewest_zero_layer == 1) * sum(fewest_zero_layer == 2)
 
